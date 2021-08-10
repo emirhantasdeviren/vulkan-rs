26488414a9acbfa5982d75480f5f498be2800c91
Emirhan Taşdeviren <emirhan.tasdeviren@outlook.com>
2021-08-10 13:35:40 +0300

    Updated example code

--- examples/development.rs
+++ examples/development.rs
@@ -1,18 +1,23 @@
 use vulkan_rs::core as vk;
 
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit::event::{Event, WindowEvent};
+use winit::platform::run_return::EventLoopExtRunReturn;
+
 fn main() {
     let app_info = vk::ApplicationInfo {
         application_name: Some(String::from("Hello Triangle")),
         application_version: vk::ApiVersion::new(0, 0, 1, 0),
         engine_name: None,
-        engine_version: vk::ApiVersion::default(),
+        engine_version: Default::default(),
         api_version: vk::ApiVersion::V1_0,
     };
     let extensions = &[
         vk::KHR_SURFACE_EXTENSION_NAME,
-        vk::KHR_XCB_SURFACE_EXTENSION_NAME,
+        vk::KHR_XLIB_SURFACE_EXTENSION_NAME,
     ];
-    let instance = vk::Instance::new(Some(&app_info), None, Some(extensions));
+    let instance = vk::Instance::new(Some(&app_info), None, Some(extensions)).unwrap();
 
     if let Some(v) = instance.version() {
         println!("Vulkan API {}", v);
@@ -23,8 +28,8 @@ fn main() {
     let physical_device = instance
         .enumerate_physical_devices()
         .into_iter()
-        .find(|p| {
-            let properties = p.properties();
+        .find(|physical_device| {
+            let properties = physical_device.properties();
             println!("{}", properties.device_name);
             properties.device_type == vk::PhysicalDeviceType::DiscreteGpu
         })
@@ -32,7 +37,27 @@ fn main() {
 
     let device = physical_device.create_device(&[0], &[&[1.0f32]]);
     let _queue = device.get_device_queue(0, 0);
-    let command_pool = device.create_command_pool(0);
-    let _command_buffers = command_pool.allocate_command_buffers(1);
-    let _semaphore = device.create_semaphore();
+
+    let mut event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_resizable(false)
+        .build(&event_loop)
+        .unwrap();
+
+    let _surface = instance.create_surface_khr(&window);
+
+    event_loop.run_return(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                println!("The close button was pressed; stopping");
+                *control_flow = ControlFlow::Exit;
+            }
+            _ => (),
+        }
+    });
 }
